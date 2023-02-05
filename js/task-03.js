ff4d8ce93f8e5b9e488fce2d4c8c480d431d3db3
Olena Savchenko <elena.volobuieva@gmail.com>
2023-02-05 15:09:13 +0300

    solves task#3

--- js/task-03.js
+++ js/task-03.js
@@ -7,15 +7,30 @@
 
 const images = [
   {
-    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'White and Black Long Fur Cat',
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
   },
   {
-    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
   },
   {
-    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Group of Horses Running',
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
   },
 ];
+
+// створює розмітку для одного li.img
+const makeImgGalleryMarkup = ({ url, alt } = {}) =>
+  `<li class="gallery__item">
+    <img class="gallery__img" src = '${url}' alt = '${alt}' width ="370">
+  </li>`;
+
+// створює массив всіх розміток із об'єкта images, перетворює масив на рядок
+const makeImgGallery = images.map(makeImgGalleryMarkup).join("");
+
+// отримує посилання на <ul class="gallery"></ul>
+const galleryRef = document.querySelector('.gallery');
+
+// метод для додавання рядка  у середину <ul class="gallery"></ul> 
+galleryRef.insertAdjacentHTML('afterbegin', makeImgGallery);
